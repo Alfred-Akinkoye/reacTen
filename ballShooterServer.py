0019e71c695ddd86f89607016de4d8df03c494a2
Julian <julian@rteng.pro>
2020-11-10 12:26:09 -0500

    feat: Including the ballShooter main class
    Declare the functions that will make up the class, specially the ones for communication with thingSpeak

---
+++ ballShooterServer.py
@@ -0,0 +1,28 @@
+"""Ball Shooter object that controls all of the associated hardware devices"""
+
+class ballShooterServer:
+    
+    """Constructor for the class"""
+    def __init__(self, ballsLeft = None, status = None):
+        self.ballsLeft = ballsLeft
+        self.status = status
+        
+    ##Public Methods
+    def status():
+        return ''
+    
+    def shootBall():
+        return False
+    
+    def startGame():
+        return False
+    
+    def finishGame():
+        return False
+    
+    ##Private Methods
+    def __sendStatus():
+        return False
+    
+    def __getMessage():
+        return False
