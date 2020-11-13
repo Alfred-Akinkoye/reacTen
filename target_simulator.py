0cabe4dfab16fa254427187ecd814a918b776fe3
delightoluwayemi <delightoluwayemi@cmail.carleton.ca>
2020-11-13 11:40:18 -0500

    updating target simulator

--- target_simulator.py
+++ target_simulator.py
@@ -12,10 +12,11 @@ channelD1write = "R9H809YX4MUSNPG1"
 channelD1read = "OX910MD5LNMRZO17"
 channelD2write = "ZDILVXVB0LFJA9AK"
 channelD2read = "ZXWNO235JBIYOK0T"
+hitwrite ="U8Y5A1WG8Y5NL3WP"
 
 def send_status():
-    status = "online"
-    params = urllib.parse.urlencode({'field1': status, 'key':channelD1write })
+    status = 0
+    params = urllib.parse.urlencode({'field2': status, 'key':channelD1write })
     headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
     conn = http.client.HTTPConnection("api.thingspeak.com:80")
     try:
@@ -28,10 +29,9 @@ def send_status():
     except:
         print ("connection failed")
 
-send_status()
 def send_hit():
     hit = True
-    params = urllib.urlencode({'field2': hit, 'key':channelD1write })
+    params = urllib.parse.urlencode({'field2': hit, 'key':hitwrite })
     headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
     conn = http.client.HTTPConnection("api.thingspeak.com:80")
     try:
@@ -46,7 +46,7 @@ def send_hit():
         
 def send_miss():
     hit = False
-    params = urllib.urlencode({'field2': hit, 'key':channelD1write })
+    params = urllib.parse.urlencode({'field2': hit, 'key':hitwrite })
     headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
     conn = http.client.HTTPConnection("api.thingspeak.com:80")
     try:
@@ -59,39 +59,19 @@ def send_miss():
     except:
         print ("connection failed")
         
-def get_start():
-    URl='https://api.thingspeak.com/channels/1160941/fields/1.json?api_key='
-    KEY=channelD1read
-    HEADER='&results=2'
-    NEW_URL=URl+KEY+HEADER
+def get_start_and_end():
+     url = 'https://api.thingspeak.com/channels/1159985/feeds.json?api_key=59EV92UFH25MFELC&results=2'
+    #print (url)
+     get_data = requests.get(url).json()
+     subject = get_data["feeds"]
+     current = subject[0]
+     print(current["field1"])
     
-    get_data = requests.get(NEW_URL,)
-    response_data = get_data.json()
-    channel_id=get_data['channel']['id']
-    
-    field_1=get_data['field1']
-    print(field_1)
-    
-def get_end():
-    URl='https://api.thingspeak.com/channels/1160941/fields/1.json?api_key='
-    KEY=channelD1read
-    HEADER='&results=2'
-    NEW_URL=URl+KEY+HEADER
-    
-    get_data = requests.get(NEW_URL,)
-    response_data = get_data.json()
-    channel_id=get_data['channel']['id']
-    
-    field_1=get_data['field1']
-    print(field_1)
-        
 def get_shootball():
     URl='https://api.thingspeak.com/channels/1160941/fields/1.json?api_key='
-    KEY=channelD1read
-    HEADER='&results=2'
-    NEW_URL=URl+KEY+HEADER
+
     
-    get_data = requests.get(NEW_URL,)
+    get_data = requests.get(NEW_URL)
     response_data = get_data.json()
     channel_id=get_data['channel']['id']
     
@@ -100,14 +80,32 @@ def get_shootball():
 
 def simulated_force():
     ballForce = random.randint(0,50)
+    print(ballForce)
     
 def hit_or_miss():
+    
     if random.randint(0,1000) <= 250:
         send_miss()
         return False
     else:
-        noOfHits += 1
         send_hit()
         simulated_force()
         return True
+def main(): 
+    get_start_and_end()
+    send_status()
+    hit_or_miss()
 
+if __name__ == "__main__":
+    main()
+    
+    
+    ##59EV92UFH25MFELC
+##https://api.thingspeak.com/channels/1180930/fields/1.json?api_key=OGZPXUJ66L9TX6M7&results=2
+ #url = 'https://api.thingspeak.com/channels/1159985/feeds.json?api_key=59EV92UFH25MFELC&results=2'
+    #print (url)
+    #get_data = requests.get(url).json()
+    #subject = get_data["feeds"]
+    #current = subject[0]
+    #print(current["field1"])
+    #U8Y5A1WG8Y5NL3WP
